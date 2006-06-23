f89dbbbcc08239bf876a21651d52989de471cf5a
nobody <nobody@47c456e0-a58a-471e-bd56-c6631cf71515>
2006-06-23 14:35:29 +0000

    This commit was manufactured by cvs2svn to create tag 'HYDRA7'.

---
+++ tags/HYDRA7/src/AS_BOG/BuildUnitigs.cc
@@ -0,0 +1,226 @@
+
+/**************************************************************************
+ * Copyright (C) 2005, J Craig Venter Institute. All rights reserved.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received (LICENSE.txt) a copy of the GNU General Public 
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *************************************************************************/
+/*************************************************
+* Module:  BuildUnitigs.c
+* Description:
+* 
+*    Programmer:  K. Li
+*       Started:  09/19/2005
+* 
+* Assumptions:
+* 
+* Notes:
+*
+*************************************************/
+
+/* RCS info
+ * $Id: BuildUnitigs.cc,v 1.10 2006-05-02 15:02:12 eliv Exp $
+ * $Revision: 1.10 $
+*/
+
+static const char BUILD_UNITIGS_MAIN_CM_ID[] = "$Id: BuildUnitigs.cc,v 1.10 2006-05-02 15:02:12 eliv Exp $";
+
+//  System include files
+
+#include<map>
+#include<set>
+#include<vector>
+#include<cmath>   // for ceil and log10
+#include<cstdlib> // for abs(int)
+#include<iostream>
+
+#include "AS_BOG_BestOverlapGraph.hh"
+#include "AS_BOG_ChunkGraph.hh"
+#include "AS_BOG_UnitigGraph.hh"
+
+using std::cout;
+using std::endl;
+using std::map;
+using std::set;
+using std::vector;
+using AS_BOG::BestOverlapGraph;
+
+//  Local include files
+extern "C" {
+#include "OlapStoreOVL.h"
+#include "AS_PER_fragStore.h"
+#include "AS_CGB_myhisto.h"
+}
+
+void outputHistograms(AS_BOG::UnitigGraph *);
+
+int  main (int argc, char * argv [])
+
+{
+   OVL_Store_t  * my_store;
+   OVL_Stream_t  * my_stream;
+   Long_Olap_Data_t  olap;
+   uint32  first,last;			// IUID of first/last fragments in olap store
+   first = 1;
+
+   fprintf(stderr, "%s\n\n", BUILD_UNITIGS_MAIN_CM_ID);
+
+   if(argc !=4){
+      fprintf(stderr, "%s: <OVL Store Path> <FRG Store Path> <genome size>\n\n", argv[0]);
+      fprintf(stderr, "  If the genome size is set to 0, this will cause the unitigger\n");
+      fprintf(stderr, "  to try to estimate the genome size based on the constructed\n");
+      fprintf(stderr, "  unitig lengths.\n");
+      fprintf(stderr, " \n");
+      return(-1);
+   }
+
+   // Get path/names of olap and frg stores from command line
+   const char* OVL_Store_Path = argv[1];
+   const char* FRG_Store_Path = argv[2];
+
+   long genome_size;
+   genome_size=atol(argv[3]);
+   std::cerr << "Genome Size: " << genome_size << std::endl;
+
+   my_store = New_OVL_Store ();
+   my_stream = New_OVL_Stream ();
+
+   // Open and initialize Overlap store
+   Open_OVL_Store (my_store, OVL_Store_Path);
+   last = Last_Frag_In_OVL_Store(my_store);
+   Init_OVL_Stream (my_stream, first, last, my_store);
+
+   // must be before creating the scoring objects, because it sets their size
+   AS_BOG::BOG_Runner bogRunner(last);
+
+   // Initialize our three different types of Best Overlap Graphs
+   //AS_BOG::ErateScore erScore;
+   //AS_BOG::LongestEdge lenScore;
+   bogRunner.push_back( new AS_BOG::LongestHighIdent(2.5));
+   bogRunner.push_back( new AS_BOG::LongestHighIdent(1.5));
+   bogRunner.push_back( new AS_BOG::LongestHighIdent(1.0));
+
+   bogRunner.processOverlapStream(my_store, my_stream, FRG_Store_Path);
+
+   ////////////////////////////////////////////////////////////////////////////
+
+   int i;
+   for(i=0; i<3; i++){
+	AS_BOG::ChunkGraph cg;
+	//cg.checkInDegree(bogRunner.metrics[i]);
+	cg.build(bogRunner.metrics[i]);
+	std::cout << "Num Fragments: " << cg.getNumFragments() << std::endl;
+	std::cout << "Num Singletons: " << cg.countSingletons() << std::endl;
+	std::cout << "Num Containees: " << bogRunner.metrics[i]->_best_containments.size() << std::endl;
+	std::cout << std::endl;
+
+	//std::cerr << er_cg << std::endl;
+	AS_BOG::UnitigGraph utg(bogRunner.metrics[i]);
+	std::cerr << "Building Unitigs.\n" << std::endl;
+	utg.build(&cg, cg.getNumFragments(), genome_size=0);
+
+	std::cerr << "Reporting.\n" << std::endl;
+	//std::cout<< utg << endl;
+	switch(i){
+		case 0:
+		utg.writeIUMtoFile("len25.ium");
+		break;
+		case 1:
+		utg.writeIUMtoFile("len15.ium");
+        outputHistograms( &utg );
+		break;
+		case 2:
+		utg.writeIUMtoFile("len10.ium");
+		break;
+	}
+	std::cerr << "///////////////////////////////////////////////////////////\n" << std::endl;
+
+    delete bogRunner.metrics[i];
+   }
+
+
+   std::cerr<<"Done with unitiger specifics.\n"<<std::endl;
+
+   ////////////////////////////////////////////////////////////////////////////
+
+   std::cerr << "Cleaning up." << std::endl;
+   // Free/clean up the frag/overlap store/stream handles
+   Free_OVL_Stream( my_stream );
+   Free_OVL_Store( my_store );
+   // Shouldn't these both be n a  destructor in BOG?
+   delete[] BestOverlapGraph::fragLength;
+
+   return  0;
+}
+
+
+void outputHistograms(AS_BOG::UnitigGraph *utg) {
+    const int nsample=500;
+    const int nbucket=500;
+    MyHistoDataType zork;
+
+    Histogram_t *length_of_unitigs_histogram = create_histogram(nsample,nbucket,0,TRUE);
+    Histogram_t *covg_histogram = create_histogram(nsample,nbucket,0,TRUE);
+    Histogram_t *arate_histogram = create_histogram(nsample,nbucket,0,TRUE);
+
+    extend_histogram(length_of_unitigs_histogram, sizeof(MyHistoDataType),
+            myindexdata,mysetdata,myaggregate,myprintdata);
+    extend_histogram(covg_histogram, sizeof(MyHistoDataType),
+            myindexdata,mysetdata,myaggregate,myprintdata);
+    extend_histogram(arate_histogram, sizeof(MyHistoDataType),
+            myindexdata,mysetdata,myaggregate,myprintdata);
+
+    AS_BOG::UnitigVector::const_iterator uiter = utg->unitigs->begin();
+    for(;uiter != utg->unitigs->end(); uiter++) {
+
+        AS_BOG::Unitig *u = *uiter;
+        zork.nsamples = 1;
+        int numFrags = u->getNumFrags();
+        zork.sum_frags = zork.min_frags = zork.max_frags = numFrags;
+
+        int bases = u->getSumFragLength();
+        zork.sum_bp = zork.min_bp = zork.max_bp = bases;
+
+        int rho = static_cast<int>(u->getAvgRho());
+        zork.sum_rho = zork.min_rho = zork.max_rho = rho;
+
+        //long covg = static_cast<long>(rint(u->getCovStat() * 1000));
+        int covg = static_cast<int>(rintf(u->getCovStat()));
+        zork.sum_discr = zork.min_discr = zork.max_discr = covg;
+
+        float arateF = u->getLocalArrivalRate() * 10000;
+        int arate = static_cast<int>(rintf(arateF));
+        if (arate < 0) {
+            std::cerr << "Negative Local ArrivalRate " << arateF << " id " << u->id
+                << " arate " << arate << std::endl;
+        }
+        zork.sum_arrival = zork.min_arrival = zork.max_arrival = arate;
+
+        zork.sum_rs_frags=zork.min_rs_frags=zork.max_rs_frags=0;
+        zork.sum_nr_frags=zork.min_nr_frags=zork.max_nr_frags=0;
+
+        add_to_histogram(length_of_unitigs_histogram, u->getLength(), &zork);
+        add_to_histogram(covg_histogram, covg, &zork);
+        add_to_histogram(arate_histogram, arate, &zork);
+    }
+    std::cerr << "Length of Unitigs histogram" << std::endl;
+    print_histogram(stderr,length_of_unitigs_histogram, 0, 1);
+    std::cerr << "Unitig Coverage Stat histogram" << std::endl;
+    print_histogram(stderr,covg_histogram, 0, 1);
+    std::cerr << "Unitig Arrival Rate histogram" << std::endl;
+    print_histogram(stderr,arate_histogram, 0, 1);
+    free_histogram( length_of_unitigs_histogram );
+    free_histogram( covg_histogram );
+    free_histogram( arate_histogram );
+}
---
+++ tags/HYDRA7/src/AS_RUN/runCA-OBT/runCAutil/createFragmentCorrectionJobs.pl
@@ -0,0 +1,101 @@
+use strict;
+
+sub createFragmentCorrectionJobs {
+    my $frgCorrBatchSize  = getGlobal("frgCorrBatchSize");
+    my $frgCorrThreads    = getGlobal("frgCorrThreads");
+    my $frgCorrOnGrid     = getGlobal("frgCorrOnGrid") && getGlobal("useGrid");
+    my $scratch           = getGlobal("scratch");
+
+    return if (getGlobal("doFragmentCorrection") == 0);
+
+    system("mkdir $wrk/2-frgcorr") if (! -e "$wrk/2-frgcorr");
+
+    return if (-e "$wrk/2-frgcorr/jobsCreated.success");
+
+    my $frgBeg = 1;
+    my $frgEnd = 0;
+
+    if ($frgCorrOnGrid) {
+        open(SUB, "> $wrk/2-frgcorr/submit.sh") or die;
+        print SUB "#!/bin/sh\n\n";
+    }
+
+    while ($frgBeg < $numFrags) {
+        $frgEnd = $frgBeg + $frgCorrBatchSize - 1;
+        $frgEnd = $numFrags if ($frgEnd > $numFrags);
+
+        #  We put 1,000,000 fragments (~12 jobs) into a single directory,
+        #  just to get them out of the way.
+
+        my $jobName   = substr("0000000000" . $frgBeg, -8);
+        my $batchName = substr($jobName, 0, 2);
+
+        system("mkdir $wrk/2-frgcorr/$batchName") if (! -d "$wrk/2-frgcorr/$batchName");
+
+        if ($frgCorrOnGrid) {
+            #  Run the correction ob on the grid.
+
+            open(F, "> $wrk/2-frgcorr/$batchName/$jobName.sh") or die;
+            print F "#!/bin/sh\n\n";
+            print F "jid=\$\$\n";
+            #print F "$processStats \\\n";
+            print F "$gin/correct-frags \\\n";
+            print F "  -S $wrk/$asm.ovlStore \\\n";
+            print F "  -o $scratch/frgcorr-$batchName-$jobName.\$jid.frgcorr \\\n";
+            print F "  $wrk/$asm.frgStore \\\n";
+            print F " $frgBeg $frgEnd \\\n";
+            print F " > $wrk/2-frgcorr/$jobName.err 2>&1 \\\n";
+            print F "&&  \\\n";
+            print F "mv $scratch/frgcorr-$batchName-$jobName.\$jid.frgcorr \\\n";
+            print F "   $wrk/2-frgcorr/$batchName/$jobName.frgcorr \\\n";
+            print F "&& \\\n";
+            print F "touch $wrk/2-frgcorr/$batchName/$jobName.success\n";
+            close(F);
+
+            chmod 0755, "$wrk/2-frgcorr/$batchName/$jobName.sh";
+
+            print SUB "qsub ";
+            print SUB "-p 0 ";  #  Priority
+            print SUB "-r y ";  #  Rerunnable
+            print SUB "-N frg_${asm}_${batchName}_$jobName ";
+            print SUB "-j y ";
+            print SUB "-o $wrk/2-frgcorr/$batchName/$jobName.grid.err ";
+            print SUB "$wrk/2-frgcorr/$batchName/$jobName.sh\n";
+        } else {
+            #  Run the correction job right here, right now.
+
+            open(F, "> $wrk/2-frgcorr/$batchName/$jobName.sh") or die;
+            print F "#!/bin/sh\n\n";
+            #print F "$processStats \\\n";
+            print F "$bin/correct-frags \\\n";
+            print F "  -t $frgCorrThreads \\\n";
+            print F "  -S $wrk/$asm.ovlStore \\\n";
+            print F "  -o $wrk/2-frgcorr/$batchName/$jobName.frgcorr \\\n";
+            print F "  $wrk/$asm.frgStore \\\n";
+            print F " $frgBeg $frgEnd \\\n";
+            print F " > $wrk/2-frgcorr/$jobName.err 2>&1 \\\n";
+            print F "&&  \\\n";
+            print F "touch $wrk/2-frgcorr/$batchName/$jobName.success\n";
+            close(F);
+
+            chmod 0755, "$wrk/2-frgcorr/$batchName/$jobName.sh";
+
+            &scheduler::schedulerSubmit("$wrk/2-frgcorr/$batchName/$jobName.sh");
+        }
+
+        $frgBeg = $frgEnd + 1;
+    }
+
+    if ($frgCorrOnGrid) {
+        close(SUB);
+        pleaseExecute("$wrk/2-frgcorr/submit.sh");
+        exit(0);
+    } else {
+        &scheduler::schedulerSetNumberOfProcesses($global{"frgCorrConcurrency"});
+        &scheduler::schedulerFinish();
+    }
+
+    touch("$wrk/2-frgcorr/jobsCreated.success");
+}
+
+1;
