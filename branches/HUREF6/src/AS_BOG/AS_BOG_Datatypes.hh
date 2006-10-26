d9941c4ffeb3335766bbcc46327133ab4fbfd491
nobody <nobody@47c456e0-a58a-471e-bd56-c6631cf71515>
2006-10-26 00:32:44 +0000

    This commit was manufactured by cvs2svn to create branch 'HUREF6'.

---
+++ branches/HUREF6/src/AS_BOG/AS_BOG_Datatypes.hh
@@ -0,0 +1,104 @@
+
+/**************************************************************************
+ * This file is part of Celera Assembler, a software program that 
+ * assembles whole-genome shotgun reads into contigs and scaffolds.
+ * Copyright (C) 1999-2004, The Venter Institute. All rights reserved.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received (LICENSE.txt) a copy of the GNU General Public 
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *************************************************************************/
+/*************************************************
+* Module:  AS_BOG_Datatypes.c
+* Description:
+*	Common datatypes
+* 
+*    Programmer:  K. Li
+*       Started:  20 July 2005
+* 
+* Assumptions:
+* 
+* Notes:
+*
+*************************************************/
+
+/* RCS info
+ * $Id: AS_BOG_Datatypes.hh,v 1.9 2006-10-08 08:47:39 brianwalenz Exp $
+ * $Revision: 1.9 $
+*/
+
+#ifndef INCLUDE_AS_BOG_DATATYPES
+#define INCLUDE_AS_BOG_DATATYPES
+
+//  System include files
+
+extern "C" {
+#include "AS_global.h"
+}
+
+namespace AS_BOG{
+
+	typedef enum { 
+		UNDEFINED,
+
+		// Dovetailing overlaps
+		DOVE_NORMAL,		// AB_AB
+			// A ----->
+			// B    ----->
+		DOVE_INNIE,		// AB_BA
+			// A ----->
+			// B    <-----
+		DOVE_OUTTIE,		// BA_AB
+			// A    ----->
+			// B <-----
+		DOVE_ANTI_NORMAL,	// BA_BA (should be same as Normal if B is rc'd)
+			// 	A <-----
+			// 	B    <-----
+			// AKA 
+			// 	A    ----->
+			// 	B  ----->
+
+		// Containment overlaps
+		CONT_NORMAL,
+			// A -------->
+			// B   --->
+		CONT_INNIE,
+			// A -------->
+			// B   <---
+		CONT_OUTTIE,
+			// A <--------
+			// B   --->
+		CONT_ANTI_NORMAL,
+			// A <--------
+			// B   <---
+	} overlap_type;
+
+	enum fragment_end_type {
+		FIVE_PRIME, 	// 5' End of fragment
+		THREE_PRIME 	// 3' End of Fragment
+	};
+
+	typedef enum {
+		UNKNOWN,
+		FORWARD,
+		REVERSE
+	} orientation_type;
+
+	typedef CDS_IID_t iuid;
+	const iuid NULL_FRAG_ID=0;
+
+} //AS_BOG namespace
+
+
+#endif
+
---
+++ branches/HUREF6/src/AS_RUN/runCA-OBT/runCAutil/preoverlap.pl
@@ -0,0 +1,117 @@
+use strict;
+
+
+sub preoverlap {
+    my @fragFiles = @_;
+
+    $numFrags = getNumberOfFragsInStore($bin, $wrk, $asm);
+
+    #  Return if there are fragments in the store, and die if there
+    #  are no fragments and no source files.
+    #
+    goto alldone if ($numFrags > 0);
+
+    die "ERROR: No fragment files specified, and stores not already created.\n" if (scalar(@fragFiles) == 0);
+
+    system("mkdir $wrk/0-preoverlap") if (! -d "$wrk/0-preoverlap");
+
+    if (! -e "$wrk/0-preoverlap/$asm.frg") {
+        my $failedFiles = 0;
+        foreach my $frg (@fragFiles) {
+            if (! -e $frg) {
+                print STDERR "MISSING: $frg\n";
+                $failedFiles++;
+            }
+        }
+        die if ($failedFiles);
+
+        #  Rather than deal with grabbing the first BAT from the frist
+        #  file (which is arbitrary anyway) we just write our own BAT.
+        #
+        #  The accession is chosen to be one before fakeUIDs used in
+        #  updating distance records.
+        #
+        open(G, "> $wrk/0-preoverlap/$asm.frg") or die;
+        print G "{BAT\n";
+        print G "bna:$asm frags\n";
+        print G "crt:0\n";
+        print G "acc:987654321987654320\n";
+        print G "com:";
+        print G "All fragments contained in:\n";
+        foreach my $frg (@fragFiles) {
+            print G "  $frg\n";
+        }
+        print G ".\n";
+        print G "}\n";
+        close(G);
+
+        #  Remove BAT and ADT messages from the input
+        #    o BAT should only be at the start of the file
+        #    o ADT (audit) should be OK to have multiple copies, but test
+        #    o _major_ performance boost if one frag file, as many things
+        #      will copy the store before adding
+
+        foreach my $frg (@fragFiles) {
+            print STDERR "$frg\n";
+
+            my $cmd = "$bin/extractmessages -x ADT BAT < $frg >> $wrk/0-preoverlap/$asm.frg";
+
+            if      ($frg =~ m/\.gz$/) {
+                $cmd = "gzip -dc $frg | $bin/extractmessages -x ADT BAT >> $wrk/0-preoverlap/$asm.frg";
+            } elsif ($frg =~ m/\.bz2$/) {
+                $cmd = "bzip2 -dc $frg | $bin/extractmessages -x ADT BAT >> $wrk/0-preoverlap/$asm.frg";
+            }
+
+            if (runCommand("$cmd 2>> $wrk/0-preoverlap/extract.err")) {
+                print STDERR "Failed.\n";
+                rename "$wrk/0-preoverlap/$asm.frg", "$wrk/0-preoverlap/$asm.frg.FAILED";
+                exit(1);
+            }
+        }
+    }
+
+    if ((! -d "$wrk/$asm.gkpStore") || (! -e "$wrk/$asm.gkpStore/gkp.frg")) {
+        my $cmd;
+        $cmd  = "cd $wrk/0-preoverlap && ";
+        $cmd .= "$bin/gatekeeper -X -C -P -e 10000000 ";
+        $cmd .= "-Q -T -N " if (getGlobal("doOverlapTrimming"));
+        $cmd .= "-f $wrk/$asm.gkpStore ";
+        $cmd .= "$wrk/0-preoverlap/$asm.frg ";
+        $cmd .= "> $wrk/0-preoverlap/gatekeeper.out ";
+        $cmd .= "2> $wrk/0-preoverlap/gatekeeper.err";
+
+        if (runCommand($cmd)) {
+            print STDERR "Failed.\n";
+            rename "$wrk/0-preoverlap/$asm.inp", "$wrk/0-preoverlap/$asm.inp.FAILED";
+            rename "$wrk/$asm.gkpStore", "$wrk/$asm.gkpStore.FAILED";
+            exit(1);
+        }
+    }
+
+    ########################################
+
+    if ((! -d "$wrk/$asm.frgStore") || (! -e "$wrk/$asm.frgStore/db.frg")) {
+        my $cmd;
+        $cmd  = "cd $wrk/0-preoverlap && ";
+        $cmd .= "$bin/PopulateFragStore -P -c -f ";
+        $cmd .= "-o $wrk/$asm.frgStore ";
+        $cmd .= "-V $wrk/0-preoverlap/$asm.ofg ";
+        $cmd .= "$wrk/0-preoverlap/$asm.inp";
+        $cmd .= "> $wrk/0-preoverlap/populatefragstore.out ";
+        $cmd .= "2> $wrk/0-preoverlap/populatefragstore.err";
+
+        if (runCommand($cmd)) {
+            print STDERR "Failed.\n";
+            rename "$wrk/0-preoverlap/$asm.ofg", "$wrk/0-preoverlap/$asm.ofg.FAILED";
+            rename "$wrk/$asm.frgStore", "$wrk/$asm.frgStore.FAILED";
+            exit(1);
+        }
+    }
+
+    $numFrags = getNumberOfFragsInStore($bin, $wrk, $asm);
+
+alldone:
+    stopAfter("initialStoreBuilding");
+}
+
+1;
