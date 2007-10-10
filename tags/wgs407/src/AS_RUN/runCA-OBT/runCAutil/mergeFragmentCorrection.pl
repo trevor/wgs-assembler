e547eddb10f493364813da50a2193d0c403f6c5b
nobody <nobody@47c456e0-a58a-471e-bd56-c6631cf71515>
2007-10-10 14:00:32 +0000

    This commit was manufactured by cvs2svn to create tag 'wgs407'.

---
+++ tags/wgs407/src/AS_RUN/runCA-OBT/runCAutil/mergeFragmentCorrection.pl
@@ -0,0 +1,51 @@
+use strict;
+
+#  Check and merge the fragment correction results
+
+sub mergeFragmentCorrection {
+
+    return if (getGlobal("doFragmentCorrection") == 0);
+
+    my $frgCorrBatchSize  = getGlobal("frgCorrBatchSize");
+
+    if (! -e "$wrk/2-frgcorr/$asm.corr") {
+        my $failedJobs = 0;
+
+        open(F, "> $wrk/2-frgcorr/cat-corrects.corrlist");
+
+        my $jobs = int($numFrags / ($frgCorrBatchSize-1)) + 1;
+        for (my $i=1; $i<=$jobs; $i++) {
+            my $frgBeg = $i * $frgCorrBatchSize - $frgCorrBatchSize + 1;
+            my $frgEnd = $i * $frgCorrBatchSize;
+            if ($frgEnd > $numFrags) {
+                $frgEnd = $numFrags;
+            }
+            $frgBeg = substr("00000000$frgBeg", -8);
+            $frgEnd = substr("00000000$frgEnd", -8);
+
+            if (! -e "$wrk/2-frgcorr/$asm-$frgBeg-$frgEnd.success") {
+                print STDERR "Fragment correction job $i ($wrk/2-frgcorr/$asm-$frgBeg-$frgEnd) failed.\n";
+                $failedJobs++;
+            }
+
+            print F "$wrk/2-frgcorr/$asm-$frgBeg-$frgEnd.frgcorr\n";
+        }
+
+        close(F);
+
+        caFailure("$failedJobs failed.  Good luck.\n") if ($failedJobs);
+            
+        my $cmd;
+        $cmd  = "$bin/cat-corrects ";
+        $cmd .= "-L $wrk/2-frgcorr/cat-corrects.corrlist ";
+        $cmd .= "-o $wrk/2-frgcorr/$asm.corr ";
+        $cmd .= "> $wrk/2-frgcorr/cat-corrects.err 2>&1";
+        if (runCommand("$wrk/2-frgcorr", $cmd)) {
+            rename "$wrk/2-frgcorr/$asm.corr", "$wrk/2-frgcorr/$asm.corr.FAILED";
+            caFailure("Failed to concatenate the fragment corrections.\n");
+        }
+    }
+    
+}
+
+1;
