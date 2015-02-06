d441eee03f51dd29418d366a37dfa8730aea6839
brianwalenz <brianwalenz@47c456e0-a58a-471e-bd56-c6631cf71515>
2015-02-06 09:30:41 +0000

    Change default lazy allocation size from 1MB to 16MB.

--- src/AS_OVM/overlapInCore-Extend_Alignment.C
+++ src/AS_OVM/overlapInCore-Extend_Alignment.C
@@ -165,7 +165,7 @@ Set_Right_Delta (int e, int d, Work_Area_t * WA) {
 //    6,710,890 to handle 80% error at   4m overlap
 //  Bigger means we can assign more than one Edit_Array[] in one allocation.
 
-uint32  EDIT_SPACE_SIZE  = 1 * 1024 * 1024;
+uint32  EDIT_SPACE_SIZE  = 16 * 1024 * 1024;
 
 static
 int *
